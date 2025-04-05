a1d74e82aa0a4f846af9a53729ab74430e94fa34
marcolaxyz7 <marcoantoniogorzoni34@gmail.com>
2025-04-04 22:43:46 -0300

    AulaBanco04/04
    final apostila 2

--- src/controllers/reservas_ambientes.js
+++ src/controllers/reservas_ambientes.js
@@ -1,4 +1,4 @@
-const db = require('../dataBase/connection');
+const bd = require('../dataBase/connection');
 
 module.exports = {
     async listarreservas_ambientes (request, response) {
@@ -15,5 +15,53 @@ module.exports = {
                dados: error.message
            });
         }
-    }
+    },
+
+    async cadastrarreservas_ambientes (request, response) {
+        try{
+           return response.status(200).json({
+               sucesso: true,
+               mensagem: 'Cadastrar reservas_ambientes.',
+               dados: null
+           });
+        } catch (error){
+           return response.status(500).json({
+               sucesso: false,
+               mensagem: 'Erro no cadastro de reservas_ambientes.',
+               dados: error.message
+           });
+        }
+    },
+
+    async editarreservas_ambientes (request, response) {
+        try{
+           return response.status(200).json({
+               sucesso: true,
+               mensagem: 'Editar reservas_ambientes.',
+               dados: null
+           });
+        } catch (error){
+           return response.status(500).json({
+               sucesso: false,
+               mensagem: 'Erro ao editar reservas_ambientes.',
+               dados: error.message
+           });
+        }
+    },
+
+    async apagarreservas_ambientes (request, response) {
+        try{
+           return response.status(200).json({
+               sucesso: true,
+               mensagem: 'Apagar reservas_ambientes.',
+               dados: null
+           });
+        } catch (error){
+           return response.status(500).json({
+               sucesso: false,
+               mensagem: 'Erro ao apagar reservas_ambientes.',
+               dados: error.message
+           });
+        }
+    },
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,8 @@ const router =  express.Router();
 const reservas_ambientesaController = require('../controllers/reservas_ambientes');
 
 router.get('/reservas_ambientes', reservas_ambientesaController.listarreservas_ambientes);
+router.post('/reservas_ambientes', reservas_ambientesaController.cadastrarreservas_ambientes);
+router.patch('/reservas_ambientes', reservas_ambientesaController.editarreservas_ambientes);
+router.delete('/reservas_ambientes', reservas_ambientesaController.apagarreservas_ambientes);
 
-modulo.exports = router;
+module.exports = router;
