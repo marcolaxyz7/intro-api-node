b5c491f83a7d2e76710d12ed40da0f1a9784a663
marcolaxyz7 <marcoantoniogorzoni34@gmail.com>
2025-04-17 20:40:22 -0300

    tentando fazer o crud do cadastrar

--- src/controllers/reserva_ambientes.js
+++ src/controllers/reserva_ambientes.js
@@ -1,66 +1,65 @@
 const db = require('../dataBase/connection');
 
 module.exports = {
-    async listarreservas_ambientes (request, response) {
+    async listarreserva_ambientes (request, response) {
 
         try{
 
             const sql = `
-            SELECT res_id, userap_id, ambiid, res_horario_inicio, res_horario_fim, res_status, res_data_reserva FROM Reservas_Ambientes;
+            SELECT res_id, userap_id, amb_id, res_horario_inicio, res_horario_fim, res_status, res_data_reserva FROM Reserva_Ambientes;
         `;
 
         const [row] = await db.query(sql);
         const nItens = row.length;
-
     
            return response.status(200).json({
                sucesso: true,
-               mensagem: 'Lista de reservas_ambientes.',
+               mensagem: 'Lista de reserva_ambientes.',
                nItens,
                dados: row
            });
         } catch (error){
            return response.status(500).json({
                sucesso: false,
-               mensagem: 'Erro na listagem de reservas_ambientes.',
+               mensagem: 'Erro na listagem de reserva_ambientes.',
                dados: error.message
            });
         }
     },
 
-    async cadastrarreservas_ambientes (request, response) {
+    async cadastrarreserva_ambientes (request, response) {
         try{
            return response.status(200).json({
                sucesso: true,
-               mensagem: 'Cadastrar reservas_ambientes.',
+               mensagem: 'Cadastrar reserva_ambientes.',
                dados: null
            });
         } catch (error){
            return response.status(500).json({
                sucesso: false,
-               mensagem: 'Erro no cadastro de reservas_ambientes.',
+               mensagem: 'Erro no cadastro de reserva_ambientes.',
                dados: error.message
            });
         }
     },
 
-    async editarreservas_ambientes (request, response) {
+    async editarreserva_ambientes (request, response) {
         try{
            return response.status(200).json({
                sucesso: true,
-               mensagem: 'Editar reservas_ambientes.',
+               mensagem: 'Editar reserva_ambientes.',
                dados: null
            });
         } catch (error){
            return response.status(500).json({
                sucesso: false,
-               mensagem: 'Erro ao editar reservas_ambientes.',
+               mensagem: 'Erro ao editar reserva_ambientes.',
                dados: error.message
            });
         }
     },
 
-    async apagarreservas_ambientes (request, response) {
+    async apagarreserva_ambientes (request, response) {
         try{
            return response.status(200).json({
                sucesso: true,
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router =  express.Router();
 
-const reservas_ambientesaController = require('../controllers/reservas_ambientes');
+const reserva_ambientesController = require('../controllers/reserva_ambientes');
 
-router.get('/reservas_ambientes', reservas_ambientesaController.listarreservas_ambientes);
-router.post('/reservas_ambientes', reservas_ambientesaController.cadastrarreservas_ambientes);
-router.patch('/reservas_ambientes', reservas_ambientesaController.editarreservas_ambientes);
-router.delete('/reservas_ambientes', reservas_ambientesaController.apagarreservas_ambientes);
+router.get('/reserva_ambientes', reserva_ambientesController.listarreserva_ambientes);
+router.post('/reserva_ambientes', reserva_ambientesController.cadastrarreserva_ambientes);
+router.patch('/reserva_ambientes', reserva_ambientesController.editarreserva_ambientes);
+router.delete('/reserva_ambientes', reserva_ambientesController.apagarreserva_ambientes);
 
 module.exports = router;
